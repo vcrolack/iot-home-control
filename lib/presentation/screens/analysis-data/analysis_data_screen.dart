7da515cc8ce16bba675467311a995611f39ad1f0
Victor Rolack <victorolack@gmail.com>
2023-11-08 21:37:18 -0300

    config router with analysis-screen

---
+++ lib/presentation/screens/analysis-data/analysis_data_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnalysisDataScreen extends StatelessWidget {
+  static const name = 'analysis-data-screen';
+
+  const AnalysisDataScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
