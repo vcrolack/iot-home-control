7f889894c1bbb4854a1d35c4c77a4507ec57f619
Victor Rolack <victorolack@gmail.com>
2023-11-28 22:40:34 -0300

    add auth provider files

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -3,3 +3,7 @@ export 'theme/theme_provider.dart';
 
 /* STORAGE PROVIDERS */
 export 'storage/local_storage_provider.dart';
+
+/* AUTH PROVIDERS */
+export 'auth/auth_provider.dart';
+export 'auth/login_form_provider.dart';
